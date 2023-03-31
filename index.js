1d9528fdad77310d1ddeca0c6cd95cf2537d6c3c
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-31 16:58:09 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+
+// function reverseString(word) {
+//   return word.split("").reverse().join("");
+// }
+
+// function isPalindrome(word) {
+//   const reversedWord = reverseString(word);
+//   return word === reversedWord;
+// }
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
 }
-
+  }
+  return true;
+}
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  If the first letter is the same as the last letter,
+   and the second letter is the same as the second to last letter,
+   and so on (until the middle of the word), the function should return true.
+
 */
 
 // You can run `node index.js` to view these console logs
